acf435c150db0d72114fcaa7176cb706e15f5e3d
anantkpatel <imagine231216@gmail.com>
2025-06-17 19:05:46 +0530

    labda

---
+++ Java New feature/Mypersona.java
@@ -0,0 +1,15 @@
+2@functionalInterface
+
+public interface  Mypersona{
+
+    
+}
+// A functional interface is an interface that contains only one abstract method. It can have multiple default
+
+
+
+package Java New feature;
+
+public class Lmabda {
+    
+}
---
+++ Java New feature/functinalInterface.md
@@ -0,0 +1,5 @@
+A functional interface contain only one abstract class
+ 
+ A lambda expression is used in java 8+ to make more readable clear consise java code
+
+ 
