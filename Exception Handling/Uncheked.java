07aad94d91d612ce04df4d613f726271324f576b
anantkpatel <imagine231216@gmail.com>
2025-06-27 17:05:08 +0530

    uncheked

---
+++ Exception Handling/Uncheked.java
@@ -0,0 +1,8 @@
+public class Uncheked {
+   public static void main(String[] args){
+    int a =5;
+    int b= 0;
+    int c= a/b; // This will cause an ArithmeticException (division by zero)
+    System.out.println("Result: " + c);
+   }
+    }
