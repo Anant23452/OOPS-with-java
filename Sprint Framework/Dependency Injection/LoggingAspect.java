1a4e93951816553a7781ecb1684d98c198a4c23f
anantkpatel <imagine231216@gmail.com>
2025-06-27 14:40:12 +0530

    aop

---
+++ Sprint Framework/Dependency Injection/LoggingAspect.java
@@ -0,0 +1,10 @@
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect{
+    @Before("execution(* com.example.service.*.*(..))")
+    public void LogBefore(){
+        System.out.println("Method execution started");
+    }
+}
