7839e78a92417e34a7ca6d8711a0e5dcf96d7b33
anantkpatel <imagine231216@gmail.com>
2025-06-28 15:33:43 +0530

    t

---
+++ Exception Handling/Thread/Mythread.java
@@ -0,0 +1,9 @@
+public class Mythread extends Thread {
+    public void run(){
+        System.out.println("Thread is running");
+    }
+    public static void main(String[] args){
+        Mythread t1 = new Mythread();// Creating a thread
+        t1.start(); // Starting the thread
+    }
+}
