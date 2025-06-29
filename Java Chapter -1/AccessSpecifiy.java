56cb86d5b5cb15e7253f2b3ccbb01fd1d06c0343
anantkpatel <imagine231216@gmail.com>
2025-06-30 00:51:22 +0530

    lkjoii

--- Java Chapter -1/AccessSpecifiy.java
+++ Java Chapter -1/AccessSpecifiy.java
@@ -11,7 +11,8 @@ void ShowAge(){
         System.out.println("Access Specifiers in Java Explained!");
         AccessSpecifiy obj = new AccessSpecifiy(); // Creating an instance of the class
         System.out.println("Name: " + obj.name); // Accessing public variable
-        // System.out.println("Age: " + obj.age); // This line would cause an error because age is private
+        // System.out.println("Age: " + obj.age);
+         // This line would cause an error because age is private
         // To access the private variable, we use a method
         obj.ShowAge(); // Calling method to show age
     }
---
+++ practice/hello.java
@@ -0,0 +1,27 @@
+
+public class hello {
+public String name = "Dileep";
+private int age = 22;
+void player(){
+   
+   
+
+}
+    public static void main(String[] args){
+      hello p=new hello();
+      System.out.println(p.name);
+      System.out.println(p.age);
+     
+      // constructor c=new constructor();
+       
+    
+    
+    } 
+
+
+  }
+  class constructor{
+    constructor(){
+      System.out.println("Anant patel");
+    }
+  }
