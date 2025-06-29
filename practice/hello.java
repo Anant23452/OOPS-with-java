df7f0a31c4f1863e7c44678bb30423b2047c9fc4
anantkpatel <imagine231216@gmail.com>
2025-06-30 01:02:33 +0530

    fgdhhg

--- practice/hello.java
+++ practice/hello.java
@@ -1,16 +1,18 @@
 
 public class hello {
-public String name = "Dileep";
+   static String college ="ABC";
+static public String name = "Dileep";
 private int age = 22;
 void player(){
    
-   
+  
 
 }
     public static void main(String[] args){
       hello p=new hello();
-      System.out.println(p.name);
+      System.out.println(name);
       System.out.println(p.age);
+      System.out.println(college);
      
       // constructor c=new constructor();
        
