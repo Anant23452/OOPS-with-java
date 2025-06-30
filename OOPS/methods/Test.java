05c3d15b97f329a34f1f26b66f704873f43fbf9a
anantkpatel <imagine231216@gmail.com>
2025-06-30 23:21:27 +0530

    [pk

--- OOPS/methods/Test.java
+++ OOPS/methods/Test.java
@@ -4,6 +4,12 @@ public class Test {
     public static void main(String[] args) {
         int[] b = {1, 2, 3, 4, 5};
         sumOfarray(b);
+
+        int add = sum(4,5);
+        System.out.println(add);
+
+        int add1 = sum(4,5,9);
+        System.out.println(add1);
         
     }
     public static void sumOfarray(int[] arr){
@@ -12,5 +18,16 @@ public class Test {
             sum+=i;
         }
         System.out.println(sum);
+    }
+
+    // Method overloading 
+    private static int sum(int a, int b){
+        return a + b;
+    }
+    private static int sum(float a, float b){
+        return a + b;
+    }
+    private static int sum (int a, int b,int c) {
+        return a+b+c;
     }
 }
