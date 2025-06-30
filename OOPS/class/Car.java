7791e6e918133f8657860e3d23c293f7f0d85e3d
anantkpatel <imagine231216@gmail.com>
2025-06-30 22:32:14 +0530

    h

--- OOPS/class/Car.java
+++ OOPS/class/Car.java
@@ -1,5 +1,5 @@
 
-class car1{
+class Car1 {
     //properties
     String color;
     int speed;
@@ -7,24 +7,24 @@ class car1{
 
     //behaviors
     //creating methods
-    void Accelerate(int increment){
-        System.out.println("Accelerating");
+    void Accelerate(int increment) {
         speed += increment;
     }
-    void Break(int decrement){
-        System.out.println("Breaking");
+    void Break(int decrement) {
         speed -= decrement;
     }
 }
-public class Car{
-   public static void main(String[] args){
-    car1 c1 = new car1();
-    c1.color = "Red";
-    c1.speed = 40;
-    c1.model = "BMW";
-    c1.Accelerate(20);
 
-    System.out.println("Car color: " + c1.speed);
+public class Car {
+    public static void main(String[] args) {
+        Car1 c1 = new Car1();
+        c1.color = "Red";
+        c1.speed = 40;
+        c1.model = "BMW";
+        c1.Accelerate(20);
 
-   }
+        System.out.println("Car color: " + c1.color);
+        System.out.println("Car speed: " + c1.speed);
+        System.out.println("Car model: " + c1.model);
+    }
 }
