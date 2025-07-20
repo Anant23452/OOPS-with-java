1ec9194c6554b3ab00c0241468b651dea040bb1c
anantkpatel <imagine231216@gmail.com>
2025-07-20 18:10:56 +0530

    multithread

---
+++ practice/multiThread.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class multiThread extends Thread {
+    
+    public static void main(String[] args) {
+        multiThread t1 = new  multiThread();
+        multiThread t2 = new multiThread();
+    }
+    
+}
